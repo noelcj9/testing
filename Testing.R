51d16904824eb571ee5d72c1ca5e6cad778c9b9e
Noel John <noelcj9@gmail.com>
2020-05-20 16:29:56 +0530

    Initial commit of Testing R.

---
+++ Testing.R
@@ -0,0 +1,5 @@
+print("this file was created within Rstudio")
+
+print("And now it lives on Github")
+
+print("And now I hope Im motivated to do this further I am really not sure about how this is gonna end up ah")
